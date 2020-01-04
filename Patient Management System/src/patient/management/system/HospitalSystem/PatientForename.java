2b74aa558c1b96687d53fb971d3f29aeae714e59
CameronDavies01 <56071558+CameronDavies01@users.noreply.github.com>
2020-01-04 20:08:46 +0000

    0.2 Project

--- Patient Management System/src/patient/management/system/HospitalSystem/PatientForename.java
+++ Patient Management System/src/patient/management/system/HospitalSystem/PatientForename.java
@@ -17,4 +17,8 @@ public class PatientForename extends Users{
         this.PatientForename = PatientForename;
         
     }
+
+    PatientForename() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
